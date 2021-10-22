e83f3e75d129472adda031c6793d011b32c6000d
Aibek <80278848+Aibek2002@users.noreply.github.com>
2021-10-22 11:53:38 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,14 +1,14 @@
 print("Дано три числа. Если ровно два из них  меньше  5, то вывести yes, иначе вывести no.")
 
 try:
-   a = int(input("Введите первое число = "))
-   b = int(input("Введите второе число = "))
-   c = int(input("Введите третье число = "))
+   q = int(input("Введите первое число = "))
+   w = int(input("Введите второе число = "))
+   e = int(input("Введите третье число = "))
 except:
    print("Tek san zhazynyz")
    exit()
 
-def aik(a):
+def aik(a,b,c):
 	if (a<5) and (b<5) and (c>5):
 		print("-yes-")
 	elif (b<5) and (c<5) and (a>5):
@@ -20,7 +20,7 @@ def aik(a):
 def zhauap():
 	print("ОТВЕТ", )
 zhauap()
-aik(a)
+aik(q,w,e)
 def paka():
 	print("Good bye!")
 paka()

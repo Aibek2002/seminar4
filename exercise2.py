0a47d746f1f2e4b245c6159faadd807333d003b3
Aibek <80278848+Aibek2002@users.noreply.github.com>
2021-10-22 12:13:22 +0500

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,1 +1,12 @@
+print("Выведите на экран числа 1, 2, 3, 4, ..., 20.")
+n=20
+def aik(a):
+        while a >0:
+                print(a)
+                a = a-1
+aik(n)
+print("Finally, n =",n)
+def poka():
+        print("Good bye!")
+poka()
 
